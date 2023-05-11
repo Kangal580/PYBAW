2e679f48d1fd18aae19b6feaf7032b2581ce9e55
Kangal580 <tooderpy4u@gmail.com>
2023-05-11 13:06:31 +0100

    Test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def option_test():
     subprocess.call(["C:\\Users\\toode\\Desktop\\Kode Test\\test.bat"], shell=True)
 
 def main():
-    print("\033[31mVelkommen til terminalen\033[0m")
+    print("\033[31mVelkommen til panelen\033[0m")
     print("_________________________")
     print("1. Test")
     print("2. Test")
