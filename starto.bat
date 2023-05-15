a7ba327fdf4c36a2ff1380baddf02bbb21fc1d48
Kangal580 <tooderpy4u@gmail.com>
2023-05-15 10:44:54 +0100

    Tilføjet: Kan nu slette temp filer? "test 2"

--- starto.bat
+++ starto.bat
@@ -1,2 +1,30 @@
 @echo off
-REM del %TEMP%\*.* /f /s /q
+
+echo Cleaning temporary files...
+
+REM Delete temporary files in the user's temp folder
+echo Deleting temporary files in the user's temp folder...
+for %%F in ("%TEMP%\*.*") do (
+    del "%%F" > nul 2>&1
+    if not errorlevel 1 (
+        echo Deleted file: %%F
+    ) else (
+        echo Unable to delete file: %%F
+    )
+)
+echo Temporary files in the user's temp folder deleted.
+
+REM Delete temporary files in the Windows temp folder
+echo Deleting temporary files in the Windows temp folder...
+for %%F in ("%windir%\Temp\*.*") do (
+    del "%%F" > nul 2>&1
+    if not errorlevel 1 (
+        echo Deleted file: %%F
+    ) else (
+        echo Unable to delete file: %%F
+    )
+)
+echo Temporary files in the Windows temp folder deleted.
+
+echo Temporary files cleaned successfully.
+pause
