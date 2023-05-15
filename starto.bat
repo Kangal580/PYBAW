3c847ead5c092b34f4923f629faf615b22118c2b
Kangal580 <tooderpy4u@gmail.com>
2023-05-15 12:00:10 +0100

    Tilføjet: Kan nu slette temp filer? "test 4"

--- starto.bat
+++ starto.bat
@@ -1,11 +1,11 @@
 @echo off
 
 del /S /q "%userprofile%\AppData\Roaming\Microsoft\Windows\Recent\*.*"
-del /S /q "%windir%\temp\*.*"
-del /S /q "%systemdrive%\$RECYCLE.BIN\*.*"
-del /S /q "%systemdrive%\temp\*.*"
-del /S /q "%temp%\*.*"
-del /S /q "%userprofile%\AppData\Local\Temp\*.*"
-del /S /q "%userprofile%\AppData\Local\Tmp\*.*"
+rd /S /q "%windir%\temp"
+rd /S /q "%systemdrive%\$RECYCLE.BIN"
+rd /S /q "%systemdrive%\temp"
+rd /S /q "%temp%"
+rd /S /q "%userprofile%\AppData\Local\Temp"
+rd /S /q "%userprofile%\AppData\Local\Tmp"
 
 pause
